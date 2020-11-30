6e015d7bc61e9a2671291cd110a12819d657d47d
Juan Tec <reluro1@gmail.com>
2020-11-30 14:35:43 -0600

    Partial release implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,19 +12,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
+
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -32,16 +23,8 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
+  
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -50,68 +33,144 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  FlutterBlue flutterBlue = FlutterBlue.instance;
+  List<BluetoothDevice> devices = new List();
+  List<BluetoothService> services;
+  BluetoothDevice device;
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      key: _scaffoldKey,
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: width/5),
+            child: Center(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    child: Image.asset(
+                      'assets/logo_login.png',
+                      width: width/2,
+                    )
+                  ),
+                  MaterialButton(
+                    onPressed: () {
+                      _scaffoldKey.currentState.showSnackBar(SnackBar(content: Text('Escaneando...')));
+                      scan();
+                    },
+                    child: Text('Escanear'),
+                    color: Colors.green
+                  ),
+                ]
+              )
+            )
+          ),
+          device != null ? Container(
+            width: width - 40,
+            margin: EdgeInsets.only(top: 40),
+            child: Column (
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  child: Text("Connected to: ${device.name}"),
+                ),
+                MaterialButton(
+                  onPressed: () {
+                    print('Sending data');
+                    // await device.write(utf8.encode(str));
+                  },
+                  child: Text('Send data'),
+                  color: Colors.green
+                )
+              ]
+            )
+          )
+          : Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 20, top: 20),
+                child: Text('Devices:', style: TextStyle(fontWeight: FontWeight.bold))
+              ),
+              Container(
+                height: height - width,
+                child: ListView.builder(
+                  itemCount: devices.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                      child: GestureDetector(
+                        onTap: () {
+                          _scaffoldKey.currentState.showSnackBar(SnackBar(content: Text('Conectando ...')));
+                          device = devices[index];
+                          connect();
+                        },
+                        child: Text("${devices[index].name} - ${devices[index].id}", maxLines: 2),
+                      )
+                    );
+                  }
+                )
+              )
+            ]
+          )
+        ]
+      )
     );
+  }
+
+  void connect() async {
+    // bool wait = true;
+    // Timer(Duration(seconds: 5), () {
+    //   wait = false;
+    // });
+    await device.connect();
+    // print('here');
+    // while(wait){
+    //   print(wait);
+    // }
+    // print('here2');
+    bool connected = (await flutterBlue.connectedDevices).length > 0;
+    if(connected)
+      _scaffoldKey.currentState.showSnackBar(SnackBar(content: Text('Conectado')));
+    else {
+      _scaffoldKey.currentState.showSnackBar(SnackBar(content: Text('No fue posible conectar')));
+      device = null;
+    }
+    setState(() {});
+  }
+
+  void scan() async {
+    if(device != null) {
+      device.disconnect();
+      device = null;
+    }
+    devices.clear();
+    try{
+      dynamic results = await flutterBlue.startScan(timeout: Duration(seconds: 5));
+      for(var i in results) {
+        // print(i.device.id);
+        if(i.device.name != "")
+          devices.add(i.device);
+      }
+    } catch(e) {
+      print(e);
+      _scaffoldKey.currentState.showSnackBar(SnackBar(content: Text('Error inesperado, intente nuevamente.')));
+    }
+    
+    setState(() {
+      if(devices.length == 0)
+        _scaffoldKey.currentState.showSnackBar(SnackBar(content: Text('No se encontraron dispositivos')));
+    });
   }
 }
